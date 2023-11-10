bad91842cc3b928f6720039b7a5e73d283e0aa37
Farruh <farruh020202@gmail.com>
2023-11-10 12:39:50 +0500

    project init

---
+++ src/components/Header/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header-left">
+        <a href="#">
+          <img src="" alt="">
+        </a>
+      </div>
+      <div class="header-right"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+import "./style/header.css"
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/shared/Button/TheButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<script>
+export default {};
+</script>
